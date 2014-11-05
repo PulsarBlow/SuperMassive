6aca634c85594c3ab143784d43269f52035ca082
PulsarBlow <pulsarblow@gmail.com>
2014-11-05 11:37:08 +0100

    Signed-off-by: PulsarBlow <pulsarblow@gmail.com>

--- GlobalAssemblyInfos.cs
+++ GlobalAssemblyInfos.cs
@@ -13,4 +13,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.1.1.0")]
 [assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1-rc")]
