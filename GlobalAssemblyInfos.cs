b3f0fc0665a6df1c459133800dbb397843316eca
PulsarBlow <pulsarblow@gmail.com>
2014-11-05 11:50:51 +0100

    Version 1.2.0

--- GlobalAssemblyInfos.cs
+++ GlobalAssemblyInfos.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1-rc")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
