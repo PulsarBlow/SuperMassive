dabc05a68dd332bca9ed0fa31a5d0d804009a5c8
PulsarBlow <pulsarblow@gmail.com>
2017-05-14 23:58:43 +0200

    Added double extension method to enable safe double equality checks

---
+++ Source/SuperMassive/Extensions/DoubleExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SuperMassive.Extensions
+{
+    using System;
+
+    public static class DoubleExtensions
+    {
+        /// <summary>
+        /// Compares two double values in a safe way (MISRA C++:2008 compliant)
+        /// </summary>
+        /// <param name="double1"></param>
+        /// <param name="double2"></param>
+        /// <param name="precision"></param>
+        /// <returns></returns>
+        public static bool AlmostEquals(this double double1, double double2, double precision = 0.0000001)
+        {
+            return Math.Abs(double1 - double2) <= precision;
+        }
+    }
+}
---
+++ Source/UnitTests/SuperMassive.Tests/Extensions/DoubleExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿namespace SuperMassive.Tests
+{
+    using NUnit.Framework;
+    using SuperMassive.Extensions;
+
+    public class DoubleExtensionsTest
+    {
+        public void DoubleExtensions_AlmostEquals_WithVariousValues_Returns_True()
+        {
+            Assert.IsTrue(0d.AlmostEquals(0d));
+            Assert.IsTrue(0d.AlmostEquals(0f));
+            Assert.IsTrue(0d.AlmostEquals(0));
+            Assert.IsTrue(1.0000001d.AlmostEquals(1.0000001f));
+            Assert.IsTrue(0.0000001d.AlmostEquals(0));
+        }
+
+        [Test]
+        public void DoubleExtensions_AlmostEquals_WithVariousValues_Returns_False()
+        {
+            Assert.IsFalse((1 / 3d).AlmostEquals(0.333d));
+            Assert.IsFalse(1d.AlmostEquals(0f));
+            Assert.IsFalse(0.0001d.AlmostEquals(0.1, 0.001d));
+        }
+    }
+}
