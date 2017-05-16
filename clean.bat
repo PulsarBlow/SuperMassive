4deffdebb00a371c46f28949592628f2a91f861c
PulsarBlow <pulsarblow@gmail.com>
2017-05-16 22:41:52 +0200

    Cleanup bat tweak

--- clean.bat
+++ clean.bat
@@ -1,8 +1,8 @@
 ECHO OFF
-git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1 --dry-run
+git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1 -e *.secret.txt --dry-run
 ECHO PROCEED AND CLEAN THE PREVIOUS LIST ?
 ECHO WARNING: THIS IS UNRECOVERABLE
 PAUSE
 ECHO CONFIRM AGAIN
 PAUSE
-git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1
+git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1 -e *.secret.txt
