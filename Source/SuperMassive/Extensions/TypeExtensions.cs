b89ebef2f53abb17ddb02935ee89bebbec023110
PulsarBlow <pulsarblow@gmail.com>
2017-05-20 09:10:47 +0200

    Adds IsNullable Type extension method

---
+++ Source/SuperMassive/Extensions/TypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperMassive.Extensions
+{
+    using System;
+
+    public static class TypeExtensions
+    {
+        public static bool IsNullable(this Type type)
+        {
+            return type != null && type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+    }
+}
