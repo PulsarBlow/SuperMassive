3bac41144e1631f564fb2baecb9a5ac764b1a047
PulsarBlow <pulsarblow@gmail.com>
2017-05-20 15:47:49 +0200

    Adds ListExtensions method and coverage

--- Source/SuperMassive/Extensions/ListExtensions.cs
+++ Source/SuperMassive/Extensions/ListExtensions.cs
@@ -8,15 +8,33 @@
     public static class ListExtensions
     {
         /// <summary>
+        /// Adds an item to the list if and only if the item is not null
+        /// </summary>
+        /// <typeparam name="T">The underlying type</typeparam>
+        /// <param name="extended">The list which is extended</param>
+        /// <param name="item">The item to add</param>
+        public static void AddIfNotNull<T>(this List<T> extended, T item)
+            where T : class
+        {
+            Guard.ArgumentNotNull(extended, nameof(extended));
+
+            if (item == null)
+                return;
+            extended.Add(item);
+        }
+
+        /// <summary>
         /// Adds a collection of items to the list if and only if the collection of items is not null
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="items"></param>
-        public static void AddRangeIfNotNull<T>(this List<T> list, IEnumerable<T> items)
+        /// <typeparam name="T">The underlying type</typeparam>
+        /// <param name="extended">The list which is extended</param>
+        /// <param name="items">The collection of items to add</param>
+        public static void AddRangeIfNotNull<T>(this List<T> extended, IEnumerable<T> items)
         {
+            Guard.ArgumentNotNull(extended, nameof(extended));
+
             if (items == null) return;
-            list.AddRange(items);
+            extended.AddRange(items);
         }
     }
 }
