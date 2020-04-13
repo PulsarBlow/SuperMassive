b583fbdc72d1ab1e30216e16ed0df68a54a9df6d
pulsarblow <pulsarblow@gmail.com>
2020-04-13 17:38:18 +0200

    chore(build): clean up useless scripts

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %~dp0
-@powershell -ExecutionPolicy Bypass -C .\build.ps1
-popd
-IF "%~1"=="-c" GOTO end
-pause
-:end
--- clean.bat
+++
@@ -1,8 +0,0 @@
-ECHO OFF
-git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1 -e *.secret.txt --dry-run
-ECHO PROCEED AND CLEAN THE PREVIOUS LIST ?
-ECHO WARNING: THIS IS UNRECOVERABLE
-PAUSE
-ECHO CONFIRM AGAIN
-PAUSE
-git clean -xfd -e *.csproj.user -e .vs -e *.suo -e *.ps1 -e *.secret.txt
